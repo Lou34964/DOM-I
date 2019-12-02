39bb5239adfa4a346340bab2c993bcf9895e8be0
Chad.Snyder <Mapletonkid@gmail.com>
2019-12-02 16:13:54 -0700

    Push 0.6

--- js/index.js
+++ js/index.js
@@ -40,6 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav bar
+
 const navs = document.getElementsByTagName('a');
 console.log(navs);
 navs[0].textContent = siteContent.nav["nav-item-1"];
@@ -48,13 +51,61 @@ navs[2].textContent = siteContent.nav["nav-item-3"];
 navs[3].textContent = siteContent.nav["nav-item-4"];
 navs[4].textContent = siteContent.nav["nav-item-5"];
 navs[5].textContent = siteContent.nav["nav-item-6"];
+
+
+
+Array.from(navs).forEach( e =>{
+  e.style.color = 'green';
+})
+
+// first section CTA
+
 const superTxt = document.querySelector('h1');
 console.log(superTxt);
 superTxt.textContent = siteContent.cta.h1;
 superTxt.style.wordSpacing = '100vw';
-const titleImg = document.querySelector('.cta img');
+const titleImg = document.querySelector('#cta-img');
 console.log(titleImg);
 titleImg.setAttribute('src', siteContent.cta["img-src"]);
 const btn = document.querySelector('.cta button');
 console.log(btn);
 btn.textContent = siteContent.cta.button;
+
+// second section Main Content
+
+const mainTxt = document.querySelectorAll('.text-content h4, .text-content p');
+console.log(mainTxt)
+const mainValues = 
+[siteContent["main-content"]["features-h4"], siteContent["main-content"]["features-content"]
+, siteContent["main-content"]["about-h4"], siteContent["main-content"]["about-content"]
+, siteContent["main-content"]["services-h4"], siteContent["main-content"]["services-content"]
+, siteContent["main-content"]["product-h4"], siteContent["main-content"]["product-content"]
+, siteContent["main-content"]["vision-h4"], siteContent["main-content"]["vision-content"]];
+console.log(mainValues);
+for (let i = 0; i < mainTxt.length; i++){
+  mainTxt[i].textContent = mainValues[i];
+}
+const mainImg = document.querySelector('#middle-img');
+console.log(mainImg);
+mainImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// Third Section Contact
+const contH4 = document.querySelector('.contact h4');
+console.log(contH4);
+contH4.textContent = siteContent.contact["contact-h4"];
+const contP = document.querySelectorAll(".contact p");
+console.log(contP);
+const contVals = [siteContent.contact.address, siteContent.contact.phone, siteContent.contact.email];
+console.log(contVals);
+for (let i = 0; i < contP.length; i++){
+  contP[i].textContent = contVals[i];
+}
+const address = document.querySelector('.contact p');
+console.log(address);
+address.style.width = '20%';
+
+// footer
+
+const copyrite = document.querySelector('footer p');
+console.log(copyrite);
+copyrite.textContent = siteContent.footer.copyright;
